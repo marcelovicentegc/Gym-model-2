e460235bfe51ef207c4af48742f23da187ffa028
Marcelo Cardoso <marcelovicentegc@gmail.com>
2018-12-27 00:07:22 -0300

    Added responsive menu

--- gym/static/gym/js/dropDown.js
+++ gym/static/gym/js/dropDown.js
@@ -1,4 +1,6 @@
 var menuBtn = document.querySelector('#dropdown-btn');
+var menuItem = document.querySelectorAll('.dropdown-options');
+var menuItemBg = document.querySelectorAll('.dropdown-options-wrapper');
 var body = document.querySelector('body');
             
 function toggleMenu() {
@@ -13,8 +15,6 @@ function toggleMenu() {
             menu.style.display = 'none'
         }, 200)
     } else {
-        // menu.style.display = 'block';
-        // menu.style.top = '100px'
         menu.style.display = 'block';
         menu.style.animationName = 'slideIn';
         menu.style.animationDuration = '.2s';
@@ -25,6 +25,7 @@ function toggleMenu() {
 
 function toggleMenuOnWindowResize() {
     var menu = document.querySelector('#dropdown');
+
     if (x.matches) {
                    
     } else {
@@ -37,6 +38,23 @@ menuBtn.addEventListener('click', function() {
     toggleMenu();
 });
 
-var x = window.matchMedia('(max-width: 1024px)');
+var x = window.matchMedia('(max-width: 1023px)');
 toggleMenuOnWindowResize(x);
 x.addListener(toggleMenuOnWindowResize);
+
+menuItem.forEach(function(item) {
+    item.addEventListener('mouseover', function() {
+        mouseOver(item.parentElement);
+    });
+    item.addEventListener('mouseout', function() {
+        mouseOut(item.parentElement);
+    });
+});
+
+function mouseOver(elem) {
+    elem.style.backgroundColor = '#000'; 
+};
+
+function mouseOut(elem) {
+    elem.style.backgroundColor = 'transparent';
+};
