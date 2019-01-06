965fa119d45d6e488db3f6ee84f3dd7ac527b123
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-01-06 12:59:41 -0300

    Updated

--- gym/static/gym/js/top.js
+++ gym/static/gym/js/top.js
@@ -1,9 +1,10 @@
 var topBtnWrapper = document.getElementById('top-btn-wrapper');
 var topBtn = document.getElementById('top-btn');
 
+var prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-    if (200 >= currentScrollPos) {
+    if (200 >= currentScrollPos && prevScrollPos > currentScrollPos) {
         topBtn.style.animationName = 'showUpOrGetOut';
         topBtn.style.animationDuration = '.2s';
         topBtn.style.animationDirection = 'reverse';
@@ -12,10 +13,6 @@ window.onscroll = function() {
             topBtn.style.fontSize = '0';
             topBtn.style.padding = '0px 0px 0px 0px';
         }, 100);
-    } else if (199 >= currentScrollPos) {
-        topBtn.style.opacity = '0';
-        topBtn.style.fontSize = '0';
-        topBtn.style.padding = '0px 0px 0px 0px';
     } else {
         topBtn.style.animationName = 'showUpOrGetOut';
         topBtn.style.animationDuration = '.2s';
@@ -24,6 +21,7 @@ window.onscroll = function() {
             topBtn.style.opacity = '1';
             topBtn.style.fontSize = '100%';
             topBtn.style.padding = '15px 15px 15px 15px';
-        }, 100);
+        }, 1);
     }
+    prevScrollPos = currentScrollPos;
 };
