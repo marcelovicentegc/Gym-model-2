6399a86fee59187c50ba4e4f70178b1db7116338
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-01-06 16:51:34 -0300

    Adding messages...

--- gym/views.py
+++ gym/views.py
@@ -1,18 +1,21 @@
 import os
 from django.shortcuts import render
-from django.views.generic.edit import FormView
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView, CreateView
 from .forms import Contact as ContactForm
 from gym.models import WhoAreWe, Service
 from django.core.mail import send_mail
+from django.contrib import messages
 
 
 
 
 
-class Home(FormView):
+class Home(FormView, CreateView):
     template_name = 'gym/home.html'
     form_class = ContactForm
     success_url = '/'
+    success_message = "Your message was succesfully delivered to our team. We'll get back in touch soon!"
 
     def get_context_data(self, **kwargs):
         kwargs['who'] = WhoAreWe.objects.latest('date')
@@ -20,8 +23,15 @@ class Home(FormView):
         return super(Home, self).get_context_data(**kwargs)
 
     def form_valid(self, form):
+        response = super().form_valid(form)
         form.send_email()
-        return super().form_valid(form)
+        success_message = self.get_success_message(form.cleaned_data)
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % cleaned_data
 
     def post(self, request):
         if request.method == 'POST':
@@ -34,7 +44,6 @@ class Home(FormView):
                 recipients = [os.environ.get('DJANGO_EMAIL_RECIPIENT')]
                 send_mail(subject=subject, message=body, from_email=sender_email, recipient_list=recipients, fail_silently=False)
                 form.save(commit=True)
+                return HttpResponseRedirect(self.success_url)
             else:
-                return render(request, self.template_name)
-            return render(request, self.template_name, {'form': form})
-        
+                return render(request, self.template_name)        
